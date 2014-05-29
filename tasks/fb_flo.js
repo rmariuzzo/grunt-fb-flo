48a2a0be005f779f227b92add7dbf7a582174f36
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-05-29 12:43:11 -0400

    Logging information added.

--- tasks/fb_flo.js
+++ tasks/fb_flo.js
@@ -10,22 +10,38 @@
 
 module.exports = function(grunt) {
 
+    // Module dependencies.
+    var flo = require('fb-flo');
+
     grunt.registerMultiTask('flo', 'Starts a fb-flo server.', function() {
 
-        var flo = require('fb-flo');
-        
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
             dir: null,
             port: 8888,
             host: 'localhost',
-            verbose: false,
+            verbose: true,
             glob: [],
             useFilePolling: false,
-            pollingInterval: null
+            pollingInterval: null,
+            resolver: function() {}
         });
 
-        var server = flo(options.dir, options);
+        var dir = options.dir;
+        var resolver = options.resolver;
+
+        delete options.dir;
+        delete options.resolver;
+
+        // Start fb-flo server
+        var server = flo(dir, options, resolver);
+
+        server.once('ready', function() {
+            var target = 'http://' + options.host + ':' + options.port;
+            grunt.log.writeln('Started fb-flo server on ' + target);
+        });
+
+        this.async();
     });
 
 };
