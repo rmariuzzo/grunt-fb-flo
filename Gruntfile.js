32c71bf0c7074a2bf32743f376786c85dba2f750
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-06-15 21:09:54 -0400

    Removed grunt clean.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,11 +23,6 @@ module.exports = function(grunt) {
             }
         },
 
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp']
-        },
-
         // Configuration to be run (and then tested).
         flo: {
             minimal_options: {
@@ -47,14 +42,10 @@ module.exports = function(grunt) {
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
 
-    // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'flo', 'nodeunit']);
+    grunt.registerTask('test', ['flo', 'nodeunit']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'test']);
